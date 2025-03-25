26f9558a28287f9160d2c380fa488dcc6debf075
202111068rajanyadav <yadavrajan276124@gmail.com>
2025-03-26 02:07:48 +0530

    Update MongoDB connection URI to remove database name from connection string

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const connectDB= async () => {
     mongoose.connection.on('connected', () => console.log('Connected to MongoDB'))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 
 export default connectDB
