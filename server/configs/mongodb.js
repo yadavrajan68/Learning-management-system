319d0f6e0bc2148ac852b5447ff58afa8e7bbcd4
202111068rajanyadav <yadavrajan276124@gmail.com>
2025-03-26 02:11:31 +0530

    Fix MongoDB connection string and correct email address extraction in clerkWebhooks

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const connectDB= async () => {
     mongoose.connection.on('connected', () => console.log('Connected to MongoDB'))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 
 export default connectDB
