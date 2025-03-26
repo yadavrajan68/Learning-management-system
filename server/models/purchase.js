c5ae7318440d20c9ade8560afa545f39fef96045
202111068rajanyadav <yadavrajan276124@gmail.com>
2025-03-27 02:25:00 +0530

    Fix typo in PurchaseSchema timestamps option

--- server/models/purchase.js
+++ server/models/purchase.js
@@ -4,12 +4,11 @@ const PurchaseSchema = new mongoose.Schema({
     courseId: { type: mongoose.Schema.Types.ObjectId,
          ref: 'Course', required: true },
     userId: { 
-     type: String,
-     //     ref: 'User', 
+     type: String, 
          required: true },
          amount: { type: Number, required: true },
          status: { type: String, enum:['pending', 'completed', 'failed'], default: 'pending' },
 
-}, {timestamp:true});
+}, {timestamps:true});
 
 export const Purchase = mongoose.model('Purchase', PurchaseSchema);
